e75ee4736024c7889d1d3cbd9724fef995f86f64
Froylan Perez <fperez@noryan.com.mx>
2019-10-24 14:28:33 -0500

    funcionalidad casi al 100, solo falta ver como hacer para q ningun juegador pueda elegir opcion hasta q los dos esten en linea

--- assets/javascript/gameOk.js
+++ assets/javascript/gameOk.js
@@ -18,9 +18,15 @@ var playerOneReady = false;
 var playerTwoReady = false;
 var evaluatePlayerOne;
 var evaluatePlayerTwo;
+var tie = 0;
+var winPlayerOne = 0;
+var winPlayerTwo = 0;
+var losePlayerOne = 0;
+var losePlayerTwo = 0;
 
 $(document).ready(function() {
     database.ref("players").remove();
+    database.ref("messages").remove();
 
 });
 
@@ -30,32 +36,41 @@ $("#startPlayerOne").on("click", function(event) {
     database.ref("/players/player1OnLine").set(
         playerOneReady,
     );  
-    $("#playerOneDisplay").css("border-color", "green");
-    $("#playerOneDisplay").css("color", "green");
-    $("#playerTwoDisplay").css("border-color", "red");
-    $("#playerTwoDisplay").css("color", "red");
-    $(".playerChoice2").css("opacity", "0.25");
+    $("#resultPlayer1").empty();
+    $("#resultPlayer2").empty();
+    database.ref("players/player2OnLine/player2Choice").remove();
+
+    // $("#playerOneDisplay").css("border-color", "green");
+    // $("#playerOneDisplay").css("color", "green");
+    // $("#playerTwoDisplay").css("border-color", "red");
+    // $("#playerTwoDisplay").css("color", "red");
+    // $(".playerChoice2").css("opacity", "0.25");
 });
 
 $("#startPlayerTwo").on("click", function(event) {
-    // playerTwoReady= true;
+    playerTwoReady= true;
     // playerOneReady = false;
     // var playerTwoGame= true;
     database.ref("/players/player2OnLine").set(
         playerTwoReady,
     );
+    $("#resultPlayer1").empty();
+    $("#resultPlayer2").empty();
+    database.ref("players/player1OnLine/player1Choice").remove();
 });
 
 $(".playerChoice1").on("click", function(snap) {
+    // $("#resultPlayer1").text("WAIT FOR YOUR OPONENT");
+    // $("#resultPlayer2").text("CHOOSE AGAIN");
     if(playerOneReady===true) {
     var choicePlayer1 = $(this).text();
     console.log(choicePlayer1);
     database.ref("/players/player1OnLine/player1Choice").set(
         choicePlayer1,
     );
-    playerOneReady = false;
-    playerTwoReady = true;
+    playerOneReady = false;      //ojo
     }
+    // playerTwoReady = true;
 });
 
 $(".playerChoice2").on("click", function(snap) {
@@ -65,15 +80,78 @@ $(".playerChoice2").on("click", function(snap) {
     database.ref("/players/player2OnLine/player2Choice").set(
         choicePlayer2,
     );
+    playerTwoReady = false;       //ojo
     }
 });
 
-// database.ref("/players").on("value", function(snapshot) {
-//     evaluatePlayerOne = snapshot.val().player1Choice;
-//     evaluatePlayerTwo = snapshot.val().player2Choice;
-//     console.log(evaluatePlayerOne);
-//     console.log(evaluatePlayerTwo);
-// });
+database.ref("/players").on("value", function(snapshot) {
+    if (snapshot.child("player1OnLine").exists() && snapshot.child("player2OnLine").exists()) {
+    // if(snapshot.val()) {
+    evaluatePlayerOne = snapshot.val().player1OnLine.player1Choice;
+    evaluatePlayerTwo = snapshot.val().player2OnLine.player2Choice;
+    console.log("eleccion jugador 1",snapshot.val());
+    console.log("eleccion jugador 2", evaluatePlayerTwo);
+    }
+    if (snapshot.child("player1OnLine/player1Choice").exists() && snapshot.child("player2OnLine/player2Choice").exists()) {
+        if (evaluatePlayerOne===evaluatePlayerTwo) {
+            console.log("Tie");
+            tie++;
+            $("#resultPlayer1").text("TIE");
+            $("#resultPlayer2").text("TIE");
+            $("#tieDisplayPlayer1").text(tie);
+            $("#tieDisplayPlayer2").text(tie);
+        }
+        if (evaluatePlayerOne==="Rock" && evaluatePlayerTwo==="Paper") {
+            losePlayerOne++;
+            winPlayerTwo++;
+            $("#resultPlayer1").text("LOSE");
+            $("#resultPlayer2").text("WIN");
+            $("#loseDisplayPlayer1").text(losePlayerOne);
+            $("#winDisplayPlayer2").text(winPlayerTwo);
+        }
+        if (evaluatePlayerOne==="Rock" && evaluatePlayerTwo==="Scissors") {
+            winPlayerOne++;
+            losePlayerTwo++;
+            $("#resultPlayer1").text("WIN");
+            $("#resultPlayer2").text("LOSE");
+            $("#winDisplayPlayer1").text(winPlayerOne);
+            $("#loseDisplayPlayer2").text(losePlayerTwo);
+        }
+        if (evaluatePlayerOne==="Paper" && evaluatePlayerTwo==="Rock") {
+            winPlayerOne++;
+            losePlayerTwo++;
+            $("#resultPlayer1").text("WIN");
+            $("#resultPlayer2").text("LOSE");
+            $("#winDisplayPlayer1").text(winPlayerOne);
+            $("#loseDisplayPlayer2").text(losePlayerTwo);
+        }
+        if (evaluatePlayerOne==="Paper" && evaluatePlayerTwo==="Scissors") {
+            losePlayerOne++;
+            winPlayerTwo++;
+            $("#resultPlayer1").text("LOSE");
+            $("#resultPlayer2").text("WIN");
+            $("#loseDisplayPlayer1").text(losePlayerOne);
+            $("#winDisplayPlayer2").text(winPlayerTwo);
+        }
+        if (evaluatePlayerOne==="Scissors" && evaluatePlayerTwo==="Rock") {
+            losePlayerOne++;
+            winPlayerTwo++;
+            $("#resultPlayer1").text("LOSE");
+            $("#resultPlayer2").text("WIN");
+            $("#loseDisplayPlayer1").text(losePlayerOne);
+            $("#winDisplayPlayer2").text(winPlayerTwo);
+        }
+        if (evaluatePlayerOne==="Scissors" && evaluatePlayerTwo==="Paper") {
+            winPlayerOne++;
+            losePlayerTwo++;
+            $("#resultPlayer1").text("WIN");
+            $("#resultPlayer2").text("LOSE");
+            $("#winDisplayPlayer1").text(winPlayerOne);
+            $("#loseDisplayPlayer2").text(losePlayerTwo);
+        }
+
+    }
+});
 
 // playersOnLine.on("value", function(snap) {
 //     var evaluatePlayerOne = database.ref("/players/player1OnLine/player1Choice");
